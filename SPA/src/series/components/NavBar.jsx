287c0d8b497c77c184deb11cf22a1c0a430aeb26
IsanorLopez <isanoralejandro@gmail.com>
2022-11-07 12:45:07 -0700

    R_SPA[ - Feat: Funcionalidad de navegacion para login con useNavigate ]

--- SPA/src/series/components/NavBar.jsx
+++ SPA/src/series/components/NavBar.jsx
@@ -1,6 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/Login', {
+            replace: true
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -40,7 +50,7 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <button className = "nav-item nav-link btn btn-danger">
+                    <button className = "nav-item nav-link btn btn-danger" onClick={ onLogout }>
                         Logout
                     </button>
                 </ul>
