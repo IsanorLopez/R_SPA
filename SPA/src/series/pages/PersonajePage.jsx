5dbb9f1c2794ca0e9ba13c18b38a70c4538e4443
IsanorLopez <isanoralejandro@gmail.com>
2022-11-07 18:02:44 -0700

    R_SPA[ - Feat: Diseno para pantalla de personaje ]

--- SPA/src/series/pages/PersonajePage.jsx
+++ SPA/src/series/pages/PersonajePage.jsx
@@ -1,19 +1,57 @@
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import { useFetchPersonaje } from '../hook';
 
 export const PersonajePage = () => {
+  
   const { Id } = useParams();
+
+  const navigate = useNavigate();
 
   const { personaje, isLoading } = useFetchPersonaje(Id);
 
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
+
   return (
     <>
+      
       {
         isLoading && <h2>Cargando...</h2>
       }
 
-      <h1>{ personaje.name }</h1>
+      <div className="row mt-5">
+        <div className="col-4 card">
+          <img 
+            src={ personaje.img }
+            alt={ personaje.name }
+            className="img"
+          />
+        </div>
+
+        <div className="col-8">
+          <h3>{ personaje.name }</h3>
+          
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item"> <b>NickName:</b> { personaje.nickname } </li>
+            <li className="list-group-item"> <b>Bithday:</b> { personaje.birthday } </li>
+            <li className="list-group-item"> <b>Status:</b> { personaje.status } </li>
+          </ul>
+
+          <h5 className="mt-3"> portrayed </h5>
+          <p>{ personaje.portrayed }</p>
+
+          <button 
+            className="btn btn-outline-primary"
+            onClick={ onNavigateBack }
+          >
+            Regresar
+          </button>
+
+        </div>
+
+      </div>
     </>
   )
 }
