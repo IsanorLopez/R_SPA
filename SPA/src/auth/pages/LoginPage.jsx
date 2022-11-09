2655cbd1763ed45ca709a1e3c5d3dd0798f67b4b
IsanorLopez <isanoralejandro@gmail.com>
2022-11-09 12:59:32 -0700

    R_SPA[ - Feat: Alerta para login incorrecto ]

--- SPA/src/auth/pages/LoginPage.jsx
+++ SPA/src/auth/pages/LoginPage.jsx
@@ -5,6 +5,7 @@ export const LoginPage = () => {
 
     const [mailValue, setMailValue] = useState('');
     const [passValue, setPassValue] = useState('');
+    const [showError, setShowError] = useState(false);
 
     const navigate = useNavigate();
 
@@ -18,34 +19,37 @@ export const LoginPage = () => {
                 replace: true
             });
         } else {
+            setShowError(true);
+            setTimeout(() => {
+                setShowError(false);
+            }, 3000);
+
             setMailValue('');
             setPassValue('');
         }
     }
 
     return (
-        <>
+        <div className="m-0 vh-100 row vh-100 justify-content-center align-items-center">
 
-            <div id="liveAlertPlaceholder" className="row w-100 justify-content-end"></div>
-
-
-            <div className="m-0 vh-100 row justify-content-center align-items-center bg-light">
-
-                <div className="col-auto text-center card shadow p-3 mb-5 bg-white rounded">
-                    
-                    <img src={ '/src/assets/logo.png' } className="card-img-top" alt="..."/>
-                    
-                    <div className="card-body">
-                        <h5 className="card-title mt-3">Sign In</h5>
+            <div className="col-auto text-center card shadow p-3 mb-5 bg-white rounded">
                         
-                        <input type="email" className="form-control mt-3" placeholder="name@example.com" value={ mailValue } onChange={ onChangeMail }/>
-                        <input type="password" className="form-control mt-3" placeholder="******" value={ passValue } onChange={ onChangePass }/>
+                <img src={ '/src/assets/logo.png' } className="card-img-top" alt="..."/>
+                        
+                <div className="card-body">
+                
+                    <h5 className="card-title mt-3">Sign In</h5>
+                            
+                    <p className="text-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}> Wrong data!!!</p>
 
-                        <a className="btn btn-primary mt-3" onClick={ () => onLogin(mailValue, passValue) }> Login </a>
-                    </div>
+                    <input type="email" className="form-control mt-3" placeholder="name@example.com" value={ mailValue } onChange={ onChangeMail }/>
+                    <input type="password" className="form-control mt-3" placeholder="******" value={ passValue } onChange={ onChangePass }/>
 
+                     <a className="btn btn-primary mt-3" onClick={ () => onLogin(mailValue, passValue) }> Login </a>
+                
                 </div>
+
             </div>
-        </>
+        </div>
     )
 }
